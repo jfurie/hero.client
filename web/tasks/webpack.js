71e4d849625049be092c5af79e55a7665c1b1bb7
Josh Furie <jfurie@gmail.com>
2015-11-09 16:40:02 -0800

    done

--- web/tasks/webpack.js
+++ web/tasks/webpack.js
@@ -4,11 +4,12 @@ var webpack = require("webpack");
 var webpackConf = require('../../webpack.web.config');
 gulp.task("webpack",['build'], function(done) {
   // run webpack
-  return webpack(webpackConf,
+  webpack(webpackConf,
     function(err, stats) {
       if (err) throw new gutil.PluginError("webpack", err);
       gutil.log("[webpack]", stats.toString({
         // output options
       }));
+      done();
     });
 });
