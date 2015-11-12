9dbf054ce13bc4f7755b0cadd8f616a53653aea4
Josh Furie <jfurie@gmail.com>
2015-11-12 15:10:14 -0800

    try again

--- webpack.web.config.js
+++ webpack.web.config.js
@@ -4,7 +4,6 @@ var path = require('path');
 var webpack = require('webpack');
 module.exports = {
   debug: true,
-  watch:true,
   devtool: 'source-map',
   entry: {
     //'index.ios': ['./src/main.ios.js'],
