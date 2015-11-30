93790fcfc9c21391f3ab158dcb0e16530c2a2908
Josh Furie <jfurie@gmail.com>
2015-11-30 12:57:41 -0800

    added material / grid/ fonts

--- src/containers/web/homeContainer.js
+++ src/containers/web/homeContainer.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { connect } from 'react-redux';
-
-@connect(state => ({
-  user: state.auth.user,
-}))
+import {Link} from 'react-router';
+import {connect} from 'react-redux';
+import {RaisedButton} from 'material-ui';
+@connect(state => ({user: state.auth.user}))
 class HomePage extends React.Component {
   constructor() {
     super();
   }
-  render() {
+  handleTouchTap(){
+    alert('tapped');
+  }
+  render () {
 
-    let {user} = this.props;
+    let {
+      user
+    } = this.props;
 
     return (
       <div>
-          {(user && user.email) ? (
-            <p>hi {user.email}</p>
-          ) : (
+        {(user && user.email)
+          ? (
+            <p>hi
+              {user.email}</p>
+          )
+          : (
             <p>hi guest</p>
           )}
         <h1>Hero.jobs</h1>
+        <RaisedButton onTouchTap={this.handleTouchTap.bind(this)} label="Default"></RaisedButton>
         <Link to="login">Login</Link>
-      </div>);
+      </div>
+    );
   }
 }
 
