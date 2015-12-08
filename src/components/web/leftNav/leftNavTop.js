606755d9e266edb98f4c486349f341af9e3cb704
Josh Furie <jfurie@gmail.com>
2015-12-07 17:01:03 -0800

    adding left nav changes

---
+++ src/components/web/leftNav/leftNavTop.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './leftNavTop.scss';
+class LeftNavTop extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <div className="leftNavTop">
+        <div className="leftNavTop-image center-xs">
+          <img
+              className="col-xs-5"
+              src="/img/profile-image.png"
+          />
+        </div>
+        <div className="leftNavTop-name">
+          Iwanna Help
+        </div>
+        <div style={{marginLeft:'.5em', marginRight:'.5em'}} className="container">
+
+
+        <div className="leftNavTop-tabs row">
+          <div className="leftNavTop-tab col-xs">
+            <div>123</div>
+              <div className="leftNavTop-label">Clients</div>
+          </div>
+          <div className="leftNavTop-tab col-xs">
+            <div>123</div>
+              <div className="leftNavTop-label">Jobs</div>
+          </div>
+          <div className="leftNavTop-tab col-xs">
+
+            <div>123</div>
+            <div className="leftNavTop-label">Candidates</div>
+          </div>
+        </div>
+              </div>
+      </div>
+    );
+  }
+}
+
+export default LeftNavTop;
