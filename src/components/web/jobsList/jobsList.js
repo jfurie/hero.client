f0c0352acb39de208b52b0fe18f653ebb2b56c77
MyBoon <romain.sylvian@gmail.com>
2015-12-07 21:12:00 -0800

    add users

--- src/components/web/jobsList/jobsList.js
+++ src/components/web/jobsList/jobsList.js
@@ -3,25 +3,77 @@ import { List, ListItem, ListDivider } from 'material-ui';
 //import Infinite from 'react-infinite';
 import { CompanyAvatar } from '../../../components/web';
 
+const style = {
+  ringUser: {
+    display: 'inline',
+    width: '32px',
+    height: '32px',
+    marginRight: '5px',
+    borderRadius: '160px',
+  },
+  peopleList: {
+    marginTop: '7px',
+  },
+};
 
 class JobsList extends React.Component {
 
   render() {
 
     let { jobs } = this.props;
-    
+
+    let people = [
+      [
+        'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+        'http://cdn.devilsworkshop.org/files/2013/01/enlarged-facebook-profile-picture.jpg',
+        'http://fe867b.medialib.glogster.com/media/92/92a0862b9fdfca48725ac9df17ea42ffd90f7bb1dcab4cac52b1f417feedd514/profile-20pic.jpg',
+        'http://static4.businessinsider.com/image/52c9702c6bb3f751704f26dd/according-to-science-these-are-the-best-photos-to-use-in-your-dating-profile.jpg',
+      ],
+      [
+        'http://cdn.devilsworkshop.org/files/2013/01/enlarged-facebook-profile-picture.jpg',
+        'http://fe867b.medialib.glogster.com/media/92/92a0862b9fdfca48725ac9df17ea42ffd90f7bb1dcab4cac52b1f417feedd514/profile-20pic.jpg',
+      ],
+      [
+        'https://cap.stanford.edu/profiles/viewImage?profileId=65672&type=square',
+        'http://fe867b.medialib.glogster.com/media/92/92a0862b9fdfca48725ac9df17ea42ffd90f7bb1dcab4cac52b1f417feedd514/profile-20pic.jpg',
+        'http://cdn.devilsworkshop.org/files/2013/01/enlarged-facebook-profile-picture.jpg',
+      ],
+      [
+        'https://cap.stanford.edu/profiles/viewImage?profileId=65672&type=square',
+      ],
+    ];
+
+    // let people1 = [];
+    // people[0].forEach(function(p, index) {
+    //   people1.push(<img key={index} style={style.ringUser} src={p} />)
+    // });
+
     // TMP
     let nestedJobsItem = [];
+    let index = 0;
 
     jobs.forEach(function(job) {
+
+      let peopleList = [];
+
+      //console.log(index, people[index]);
+
+      people[index].forEach(function(p, key) {
+        peopleList.push(<img key={key} style={style.ringUser} src={p} />)
+      });
+
       nestedJobsItem.push(
         <ListItem
           primaryText={job.get('title')}
-          secondaryText={<p>lorem ipsum</p>}
+          secondaryText={
+            <div style={style.peopleList}>{peopleList}</div>
+          }
           secondaryTextLines={2}
           key={job.get('id')}
         />
       );
+
+      index += 1;
     });
 
     return (
