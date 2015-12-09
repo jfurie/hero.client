e3c85e4377cd835d780d9590ae58cbe043373ebe
Josh Furie <jfurie@gmail.com>
2015-12-08 21:23:25 -0800

    added image / action buttons

--- src/containers/web/jobs/jobDetailsContainer.js
+++ src/containers/web/jobs/jobDetailsContainer.js
@@ -4,9 +4,10 @@ import { getOneJob } from '../../../modules/jobs';
 import { getAllContacts } from '../../../modules/contacts';
 import { getOneLocation } from '../../../modules/locations';
 import { Header, ContactsList, LocationCard} from '../../../components/web';
-import {Styles,  IconMenu, IconButton, Tabs, Tab,List , ListItem, FontIcon, Card, CardHeader, Avatar, CardText, CardTitle} from 'material-ui';
+import {Styles,  IconMenu, IconButton, Tabs, Tab,List , ListItem, FontIcon, Card, CardHeader, Avatar, CardText, CardTitle, CardMedia, FlatButton, RaisedButton} from 'material-ui';
 import { disableSwipeToOpen, enableSwipeToOpen } from '../../../modules/leftNav';
 import SwipeableViews from 'react-swipeable-views';
+import './jobDetailsContainer.scss';
 let MenuItem = require('material-ui/lib/menus/menu-item');
 
 const style = {
@@ -84,75 +85,101 @@ class JobDetailsPage extends React.Component {
       <div>
         <Header iconRight={
           <IconMenu iconButtonElement={
-            <IconButton  iconClassName="material-icons">more_vert</IconButton>
+            <IconButton  iconClassName='material-icons'>more_vert</IconButton>
           }>
-            <MenuItem index={0} primaryText="Find Candidates" />
+            <MenuItem index={0} primaryText='Find Candidates' />
           </IconMenu>
         } title={job?job.get('title'):''} />
         <Tabs tabItemContainerStyle={style.tabs} onChange={this._handleChangeTabs.bind(this)} value={this.state.slideIndex + ''}>
-          <Tab label="Details" value="0"></Tab>
-          <Tab label="Description" value="1"></Tab>
-          <Tab label="Candidates" value="2"></Tab>
-          <Tab label="Notes" value="3"></Tab>
+          <Tab label='Details' value='0'></Tab>
+          <Tab label='Description' value='1'></Tab>
+          <Tab label='Candidates' value='2'></Tab>
+          <Tab label='Notes' value='3'></Tab>
         </Tabs>
         <SwipeableViews resitance index={this.state.slideIndex} onChangeIndex={this._handleChangeIndex.bind(this)}>
           <div style={style.slide}>
             <Card>
-              <List>
-                  <ListItem
-                    leftIcon={<FontIcon className="material-icons">timeline</FontIcon>}
-                    primaryText={<p style={{'color':'green'}}>Active</p>}
-                    secondaryText={<p>status</p>}
-                    secondaryTextLines={1}
-                    disabled={true}
-                  />
-                  <ListItem
-                    leftIcon={<FontIcon className="material-icons">people</FontIcon>}
-                    primaryText={<p style={{'color':'green'}}>25</p>}
-                    secondaryText={<p>applicants</p>}
-                    secondaryTextLines={1}
-                    disabled={true}
-                  />
-                  <ListItem
-                    leftIcon={<FontIcon className="material-icons">date_range</FontIcon>}
-                    primaryText={<p style={{'color':'green'}}>25</p>}
-                    secondaryText={<p>days open</p>}
-                    secondaryTextLines={1}
-                    disabled={true}
-                  />
-              </List>
+            {/*  <div className="mediawrap">
+                <CardMedia overlay={
+                    <div className='row'>
+                      <div className='col-xs-9'>
+                        <CardTitle titleStyle={{color:'rgba(255, 255, 255, 0.87)'}} subtitleStyle={{color:'rgba(255, 255, 255, 0.54)'}} title="Andriod Mobile Engineer" subtitle="FreedomPop"/>
+
+                        </div>
+                      <div className='col-xs-3'>
+                        <div className="button-right-bottom">
+                        <RaisedButton label="Apply" primary={true} />
+                        </div>
+                      </div>
+                    </div>
+                  }>
+                  <img src='https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e15/11378685_847466158664846_945103283_n.jpg'></img>
+                </CardMedia>
+              </div>*/}
+                <div className="mediawrap">
+                <CardMedia>
+                  <img src='https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e15/11378685_847466158664846_945103283_n.jpg'></img>
+                </CardMedia>
+                <div className="button-right-bottom">
+
+                  <FlatButton className='ghost' style={{backgroundColor:'rgba(0,0,0,0.70)',border:'1px solid rgba(255,255,255,0.70)', color:'rgba(255,255,255,0.97)', borderRadius:'5px'}} label="Apply" />
+                </div>
+                <div className="button-left-bottom">
+
+                  <FlatButton className='ghost' style={{backgroundColor:'rgba(0,0,0,0.70)',border:'1px solid rgba(255,255,255,0.70)', color:'rgba(255,255,255,0.97)', borderRadius:'5px'}}  label="Share">
+                  </FlatButton>
+                </div>
+              </div>
+              <CardText>
+                <Card>
+                  <CardText>
+                    <div className='row center-xs'>
+                      <div style={{fontSize:'16px','color':'green'}} className='col-xs-4'>140k <div style={{fontSize:'11px','color':'rgba(0,0,0,0.54)'}}>salary</div></div>
+                      <div style={{fontSize:'16px','color':'green'}} className='col-xs-4'>Permanent <div style={{fontSize:'11px','color':'rgba(0,0,0,0.54)'}}>position</div></div>
+                      <div style={{fontSize:'16px','color':'green'}} className='col-xs-4'>25 <div style={{fontSize:'11px','color':'rgba(0,0,0,0.54)'}}>applicants</div></div>
+                    </div>
+                  </CardText>
+
+                </Card>
+              </CardText>
               <CardText >
-                <p>
-                  Telecom startup disrupting a $100B industry with free mobile and home internet service. Funded by the founder of Skype. Java/Spring/JPA/Hibernate/Redis; *Now requires experience with a client-side JavaScript MVC framework (Angular, React, Backbone, Ember)
-                </p>
+                <div className='description'>
+                  <p >
+                    Telecom startup disrupting a $100B industry with free mobile and home internet service. Funded by the founder of Skype. Java/Spring/JPA/Hibernate/Redis; *Now requires experience with a client-side JavaScript MVC framework (Angular, React, Backbone, Ember)
+                  </p>
+                </div>
+
+              </CardText>
+              <CardText >
+                            <LocationCard style={{height: '200px'}} location={location} />
               </CardText>
 
               <CardText>
                 <Card>
-                  <List subheader="Compensation Details">
+                  <List subheader='Compensation Details'>
                       <ListItem
-                        leftIcon={<FontIcon className="material-icons">attach_money</FontIcon>}
+                        leftIcon={<FontIcon className='material-icons'>attach_money</FontIcon>}
                         primaryText={'$150,000'}
                         secondaryText={<p>salary</p>}
                         secondaryTextLines={1}
                         disabled={true}
                       />
                       <ListItem
-                        leftIcon={<FontIcon className="material-icons">attach_money</FontIcon>}
+                        leftIcon={<FontIcon className='material-icons'>attach_money</FontIcon>}
                         primaryText={'20%'}
                         secondaryText={<p>fee</p>}
                         secondaryTextLines={1}
                         disabled={true}
                       />
                       <ListItem
-                        leftIcon={<FontIcon className="material-icons">attach_money</FontIcon>}
+                        leftIcon={<FontIcon className='material-icons'>attach_money</FontIcon>}
                         primaryText={'$30,000'}
                         secondaryText={<p>estimated fee</p>}
                         secondaryTextLines={1}
                         disabled={true}
                       />
                       <ListItem
-                        leftIcon={<FontIcon className="material-icons">person</FontIcon>}
+                        leftIcon={<FontIcon className='material-icons'>person</FontIcon>}
                         primaryText={'Permanent'}
                         secondaryText={<p>position type</p>}
                         secondaryTextLines={1}
@@ -162,8 +189,8 @@ class JobDetailsPage extends React.Component {
                 </Card>
               </CardText>
 
-              <LocationCard style={{height: '200px'}} location={location} />
-            <List subheader="Job Contact">
+
+            <List subheader='Job Contact'>
               {(heroContact) ? (
                 <ListItem
                   leftAvatar={<Avatar>S</Avatar>}
@@ -173,7 +200,7 @@ class JobDetailsPage extends React.Component {
                 />
               ) : (null)}
               </List>
-              <List subheader="Your HERO talent advocate">
+              <List subheader='Your HERO talent advocate'>
                 {(heroContact) ? (
                   <ListItem
                     leftAvatar={<Avatar src={heroContact} />}
@@ -188,7 +215,13 @@ class JobDetailsPage extends React.Component {
 
           </div>
           <div style={style.slide}>
-            1
+            <Card>
+              <CardText >
+                <div className='description'>
+        <h3 className='c2'><a name='h.2qn7ea1nr273' /><span>Job Description</span></h3><p className='c4'><span className='c0'>Our client is a first-of-its-kind technology startup that is disrupting a multi-billion dollar industry. They have a significant amount of funding, and their backers include founders of companies that have become household names. They are currently hiring for a Senior Software Engineer who has a background building high-traffic, multi-threaded software systems using MVC frameworks like and and has strong relational and ideally database experience.</span></p><h4 className='c2'><a name='h.usesbgd80cl3' /><span>The non-negotiables:</span></h4><ul className='c7 lst-kix_p70ulp80wgsv-0 start'><li className='c4 c5'><span className='c0'>&nbsp;5+ years of Java Development experience</span></li><li className='c4 c5'><span className='c0'>Experience with a client-side JavaScript MVC framework (Angular, React, Backbone, Ember)</span></li><li className='c4 c5'><span className='c0'>Expertise with Spring and Hibernate / JPA</span></li><li className='c4 c5'><span className='c0'>&nbsp;Experience with relational databases like SQL Server, MySQL</span></li><li className='c4 c5'><span className='c0'>Exposure to technologies, i.e. preferred</span></li><li className='c4 c5'><span className='c0'>A Bachelor's degree in Computer Science or greater</span></li></ul><h4 className='c2'><a name='h.1klbg0xnecvg' /><span>The reasons to work there:</span></h4><ul className='c7 lst-kix_ue7i54lkmb8h-0 start'><li className='c4 c5'><span className='c0'>Competitive compensation package with excellent benefits</span></li><li className='c4 c5'><span className='c0'>The chance to put your stamp on a platform that's already used by millions of people and gaining thousands of users every day</span></li><li className='c4 c5'><span className='c0'>The stability of a well-funded company with traction, with the excitement of a startup</span></li><li className='c4 c5'><span className='c0'>Huge growth opportunity</span></li></ul><p className='c1'><span className='c0' /></p><p className='c4'><span className='c0'>If this fits your background and you're ready to make a huge impact at a small company, apply now. We look forward to speaking with you soon.</span></p><p className='c4'><span className='c0'>Local candidates are strongly preferred. Sponsorship is not an option at this time.</span></p><p className='c6'><span /></p>
+      </div>
+              </CardText>
+            </Card>
           </div>
           <div style={style.slide}>
             <ContactsList contacts={contacts.list} />
