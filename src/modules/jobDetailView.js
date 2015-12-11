225b9abc38ac23afb980100ff9aec9ff5d2c801d
Josh Furie <jfurie@gmail.com>
2015-12-10 17:45:54 -0800

    remove unused file

--- src/modules/jobDetailView.js
+++
@@ -1,4 +0,0 @@
-import Immutable from 'immutable';
-
-const OPEN_CONTACT_MODAL = 'hero.client/jobDetailView/OPEN_CONTACT_MODAL';
-const CLOSE_CONTACT_MODAL = 'hero.client/jobDetailView/CLOSE_CONTACT_MODAL';
