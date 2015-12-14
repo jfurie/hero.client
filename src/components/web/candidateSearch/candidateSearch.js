90be3031b39f72eb82c9c4dac350903888bf6cff
Josh Furie <jfurie@gmail.com>
2015-12-14 13:49:46 -0800

    Candidate search

---
+++ src/components/web/candidateSearch/candidateSearch.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, CardText, TextField, IconButton} from 'material-ui';
+export default class CandidateSearch extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+        <Card>
+        <CardText>
+          <TextField
+            fullWidth
+            floatingLabelText="Search Candidates"
+            >
+          </TextField>
+          <TextField
+            fullWidth
+            floatingLabelText="Location"
+            >
+          </TextField>
+          <div>
+            <IconButton iconClassName="material-icons" tooltipPosition="top-center"
+          tooltip="View List">view_list</IconButton> |
+            <IconButton iconClassName="material-icons" tooltipPosition="top-center"
+  tooltip="View Map">location_on</IconButton> |
+  <IconButton iconClassName="material-icons" tooltipPosition="top-center"
+tooltip="View Favorites">favorite</IconButton>
+          </div>
+        </CardText>
+      </Card></div>
+
+    );
+  }
+}
