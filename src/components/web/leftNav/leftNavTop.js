af7be2867d72a8d059c56d800abdf533490586ef
MyBoon <romain.sylvian@gmail.com>
2016-01-26 03:11:00 -0800

    fix left bar center issues

--- src/components/web/leftNav/leftNavTop.js
+++ src/components/web/leftNav/leftNavTop.js
@@ -53,11 +53,36 @@ class LeftNavTop extends React.Component {
 
     if (contact && stats) {
       return (
-        <div className="leftNavTop">
-          <div className="leftNavTop-image center-xs">
-            <Gravatar style={style.gravatar} email={contact.email} />
+        <div className="leftNavTop row center-xs">
+          <div className="leftNavTop-image col-xs-12">
+            <div className='box'>
+              <Gravatar style={style.gravatar} email={contact.email} />
+            </div>
           </div>
-          <div className="leftNavTop-name">
+          <div className="leftNavTop-name col-xs-12">
+            <div className='box'>
+              <p>{contact.displayName}</p>
+            </div>
+          </div>
+          <div className="leftNavTop-tab col-xs-4" style={{marginLeft: '-12px'}}>
+            <div className="box">
+              <div>{stats.companyCount}</div>
+              <div className="leftNavTop-label">Clients</div>
+            </div>
+          </div>
+          <div className="leftNavTop-tab col-xs-4">
+            <div className="box">
+              <div>{stats.jobCount}</div>
+              <div className="leftNavTop-label">Jobs</div>
+            </div>
+          </div>
+          <div className="leftNavTop-tab col-xs-4">
+            <div className="box">
+              <div>{stats.candidateCount}</div>
+              <div className="leftNavTop-label">Candidates</div>
+            </div>
+          </div>
+          {/*<div className="leftNavTop-name">
           {contact.displayName}
           </div>
           <div style={{margin:'auto', width: 'auto'}} className="container">
@@ -75,7 +100,7 @@ class LeftNavTop extends React.Component {
                 <div className="leftNavTop-label">Candidates</div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       );
     }
