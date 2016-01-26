5353f8a003e7aa1582750b5d716921fec8eddd5e
MyBoon <romain.sylvian@gmail.com>
2016-01-25 17:28:54 -0800

    [#112253919 fix] header on find candidates

--- src/components/web/candidateSearch/candidateSearchModal.js
+++ src/components/web/candidateSearch/candidateSearchModal.js
@@ -25,6 +25,8 @@ const style = {
   toolbar: {
     backgroundColor: '#ffffff',
     height: '64px',
+    position: 'fixed',
+    zIndex: '10',
   },
   toolbarIcon: {
     marginTop:'8px',
@@ -40,7 +42,10 @@ const style = {
   toolbarTitle: {
     lineHeight:'64px',
     float:'left',
-  }
+  },
+  box: {
+    marginTop: '67px',
+  },
 };
 
 export default class CandidateSearchModal extends React.Component {
@@ -52,36 +57,6 @@ export default class CandidateSearchModal extends React.Component {
   render() {
 
     let clientHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-
-    /*<div>
-      <Dialog
-          open={this.props.open}
-          autoDetectWindowHeight={false}
-          autoScrollBodyContent={false}
-          repositionOnUpdate={false}
-          defaultOpen={false}
-          style={style.dialog}
-          bodyStyle={style.bodyStyle}
-          contentStyle={style.contentStyle}
-      >
-          <div style={{
-            position:'fixed',
-            width:'100%',
-          }}>
-            <Toolbar style={{backgroundColor:'#ffffff', height:'64px'}}>
-              <ToolbarGroup key={0} float="left">
-                <IconButton onTouchTap={this.props.close.bind(this)} style={{marginTop:'8px',float:'left', marginRight:'8px', marginLeft:'-16px'}} iconClassName='material-icons'>close</IconButton>
-                <ToolbarTitle style={{lineHeight:'64px', float:'left'}} text="Find Candidates" />
-              </ToolbarGroup>
-              <ToolbarGroup key={1} float="right">
-              </ToolbarGroup>
-            </Toolbar>
-          </div>
-          <div style={{height:'64px'}}></div>
-        <CandidateSearch {...this.props}></CandidateSearch>
-      </Dialog>
-    </div>*/
 
     return (
 
@@ -102,9 +77,9 @@ export default class CandidateSearchModal extends React.Component {
               <ToolbarTitle style={style.toolbarTitle} text="Find Candidates" />
             </ToolbarGroup>
           </Toolbar>
-          <div className="row center-xs">
-              <div className="col-xs-10 col-md-6">
-                  <div className="box">
+          <div className="row">
+              <div className="col-xs-12 col-md-10">
+                  <div className="box" style={style.box}>
                     <CandidateSearch {...this.props}></CandidateSearch>
                   </div>
               </div>
