bfbd3c08c2f8235d3290432dd024a43023b0b296
MyBoon <romain.sylvian@gmail.com>
2016-02-09 17:38:44 -0800

    add proptypes to Tag

--- src/components/web/Tag/tag.js
+++ src/components/web/Tag/tag.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import _ from 'lodash';
 
-let style = {
+const style = {
   badge:{
-    fontSize:'14px',
+    fontSize: '14px',
     lineHeight: '18px',
     color: 'rgba(255, 255, 255, 1.00)',
-    backgroundColor:'#E91B62',
-    borderRadius:'6px',
-    display:'inline-block',
-    padding:'0px 16px',
-    marginBottom:'8px',
-    height:'18px'
+    backgroundColor: '#E91B62',
+    borderRadius: '6px',
+    display: 'inline-block',
+    padding: '0px 16px',
+    marginBottom: '8px',
+    height: '18px',
   },
 };
+
 export default class Tag extends React.Component {
+
   _onTouchTap(...args) {
-    if(this.props.onTouchTap){
+    if (this.props.onTouchTap) {
       this.props.onTouchTap(...args);
     }
   }
-  render(){
-    let {value} = this.props;
-    let currentStyle =_.merge(style,this.props.style);
+
+  render() {
+
+    let { value } = this.props;
+    let currentStyle = _.merge(style, this.props.style);
 
     return (
       <span onTouchTap={this._onTouchTap.bind(this)} style={currentStyle.badge} >{value}!</span>
     );
   }
 }
+
+Tag.propTypes = {
+  onTouchTap: React.PropTypes.func,
+  value: React.PropTypes.string.isRequired,
+};
