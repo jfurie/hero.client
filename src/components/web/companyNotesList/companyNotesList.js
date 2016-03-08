809333adeb8088468998bb5c393570e3c2379557
thiensly <thiensly@gmail.com>
2016-03-08 11:18:27 -0800

    new notes ui

--- src/components/web/companyNotesList/companyNotesList.js
+++ src/components/web/companyNotesList/companyNotesList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Divider, Card, CardHeader, CardText, CardActions, FlatButton } from 'material-ui';
+import { IconMenu, IconButton, MenuItem } from 'material-ui';
 import { Gravatar } from '../../../components/web';
 import TimeAgo from 'react-timeago';
 import Immutable from 'immutable';
@@ -35,23 +35,22 @@ class CompanyNotesList extends React.Component {
 
         return (
           <div>
-          <Card style={{marginLeft:'8px',marginRight:'8px',marginBottom:'8px'}} initiallyExpanded>
-            <CardHeader
-              title={contact.get('displayName')}
-              subtitle={<div>{(note.get('privacyValue') ? 'Public' : 'Private')} | <TimeAgo live={true} date={note.get('updated')} /></div>}
-              avatar={<Gravatar style={style.gravatar} email={contact.get('email')} />}>
-            </CardHeader>
-            <CardText expandable>
-              {note.get('noteText')}
-
-            </CardText>
-            <CardActions expandable>
-              <FlatButton label="Edit" onTouchTap={self.editNote.bind(self, note)} />
-              <FlatButton label="Delete" onTouchTap={self.deleteNote.bind(self, note)} />
-            </CardActions>
-          </Card>
-
-            <Divider inset />
+            <div style={{display:'flex', position: 'relative', marginBottom: '15px'}}>
+              <div style={{flex:'0 0 56px', marginLeft: '15px'}}>
+                <Gravatar style={style.gravatar} email={contact.get('email')}/>
+              </div>
+              <div style={{flexGrow: 1, borderBottom: '1px solid #ddd', paddingBottom: '10px'}}>
+                  <div style={{margin: '5px 0', color:'rgba(0, 0, 0, 0.87)', marginTop: 0, fontSize:'15px', fontWeight:'500'}}>{contact.get('displayName')}</div>
+                  <div style={{margin: '5px 0', color:'rgba(0, 0, 0, 0.87)', fontSize:'14px', whiteSpace:'pre'}}>{note.get('noteText')}</div>
+                  <div style={{margin: '5px 0', color:'rgba(0, 0, 0, 0.54)', fontSize:'14px'}}><TimeAgo live={true} date={note.get('updated')} /></div>
+              </div>
+              <div style={{position: 'absolute', right: '10px'}}>
+                <IconMenu iconButtonElement={<IconButton  iconClassName="material-icons">more_vert</IconButton>}>
+                  <MenuItem index={0} onTouchTap={self.editNote.bind(self, note)} primaryText="Edit" />
+                  <MenuItem index={0} onTouchTap={self.deleteNote.bind(self, note)} primaryText="Delete" />
+                </IconMenu>
+              </div>
+            </div>
           </div>
         );
       })}
