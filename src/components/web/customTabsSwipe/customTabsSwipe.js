026057bd62fc7488fa070259fb8584895aa40508
thiensly <thiensly@gmail.com>
2016-03-16 12:09:21 -0700

    tab state fix

--- src/components/web/customTabsSwipe/customTabsSwipe.js
+++ src/components/web/customTabsSwipe/customTabsSwipe.js
@@ -49,11 +49,11 @@ class CustomTabsSwipe extends React.Component {
 
     if (slideIndex < 0) slideIndex = 0;
 
-    this.state = {
+    let clientWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    this.state= {
       slideIndex,
+      'windowWidth':clientWidth,
     };
-    var clientWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    this.state= {'windowWidth':clientWidth};
     this.handleResize = this.handleResize.bind(this);
   }
 
@@ -204,7 +204,7 @@ class CustomTabsSwipe extends React.Component {
 CustomTabsSwipe.propTypes = {
   isLight: React.PropTypes.bool,
   onSwipeEnd: React.PropTypes.func,
-  startingTab: React.PropTypes.number,
+  startingTab: React.PropTypes.string,
   tabs: React.PropTypes.array.isRequired,
 };
 
