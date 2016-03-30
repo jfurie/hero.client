63dc12a4f120ed1531908d6af08206d67d2c7fc2
Josh Furie <jfurie@gmail.com>
2016-03-30 13:11:22 -0700

    remove extra description

--- src/components/web/publicJob/publicJob.js
+++ src/components/web/publicJob/publicJob.js
@@ -167,6 +167,7 @@ export default class PublicJob extends React.Component {
                       />
                     </div>
                     <div style={{width: '100%', marginRight: '8px'}}>
+                      <div style={style.title}>{'Description'}</div>
                       <div className="description">
                         <div dangerouslySetInnerHTML={{__html: description}} />
                       </div>
@@ -218,29 +219,6 @@ export default class PublicJob extends React.Component {
                 </CardText>
               ):(<span></span>)}
             </Card>
-            {
-              description ?
-              <Card>
-                <CardText>
-                  <div style={{display:'flex'}}>
-                    <div style={{flex:'0 0 56px'}}>
-                      <Avatar
-                          icon={<FontIcon className="material-icons">stars</FontIcon>}
-                          color={Styles.Colors.grey600}
-                          backgroundColor={Styles.Colors.white}
-                      />
-                    </div>
-                    <div style={{width: '100%', marginRight: '8px'}}>
-                      <div style={style.title}>{'Description'}</div>
-                      <div className="description">
-                        <div dangerouslySetInnerHTML={{__html: description}} />
-                      </div>
-                    </div>
-                  </div>
-                </CardText>
-              </Card>
-              : (null)
-            }
             {
               talentAdvocate ?
               <Card>
