ad1c16f2ec7be40802150bd68fd9d9fd90a71af9
thiensly <thiensly@gmail.com>
2016-04-05 15:34:32 -0700

    fix wrong dev heroAccount

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 export default {
   apiBaseUrl:'https://core-api-loopback-dev.herokuapp.com',
-  heroAccountId:'564fafd0708b5103001b4ea0',
+  heroAccountId:'5643ceff105cccd23af7b1a1',
   heroCompanyId:'568f0ea89faa7b2c74c18080',
-}
+};
