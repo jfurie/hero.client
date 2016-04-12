4cfa17aa48a34ac71e9410f727ef18cc5d5dcf75
Josh Furie <jfurie@gmail.com>
2016-04-11 17:45:27 -0700

    indent

--- src/components/web/contactSearch/contactSearch.js
+++ src/components/web/contactSearch/contactSearch.js
@@ -43,8 +43,8 @@ const style = {
 };
 
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
 export default class ContactSearch extends React.Component {
